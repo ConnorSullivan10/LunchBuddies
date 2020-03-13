5a5d5953d9a0bf0024b65bbfbb3fb4b24362e93d
Connor Sullivan <cksullvn@gmail.com>
2020-03-13 13:08:39 -0500

    parts 1 and 2 pending completion

---
+++ LunchBuddies/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchBuddies
+{
+    class Restaurant
+    {
+        public string Name { get; set; } = "";
+        private List<string> Restaurants { get; } = new List<string>();
+
+        public Restaurant(List<string> Restaurants)
+        {
+            var random = new Random();
+            int index = random.Next(Restaurants.Count);
+            string randString = Restaurants[index];
+            Name = randString;
+        }
+
+        public void eat()
+        {
+
+        }
+    }
+}
