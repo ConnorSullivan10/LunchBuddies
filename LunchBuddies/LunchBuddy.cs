14873a71107eb580098cd6a7ca3193d52f1009be
Connor Sullivan <cksullvn@gmail.com>
2020-03-13 11:18:50 -0500

    setup file

---
+++ LunchBuddies/LunchBuddy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchBuddies
+{
+    class LunchBuddy
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public LunchBuddy(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
